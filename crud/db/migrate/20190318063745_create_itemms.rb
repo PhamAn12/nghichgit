d7603e7edd6f9c5dd8d86377d7452d876a6a83f9
PhamAn12 <phamkhacan1903@gmail.com>
2019-03-18 14:23:14 +0700

    init commit

---
+++ crud/db/migrate/20190318063745_create_itemms.rb
@@ -0,0 +1,10 @@
+class CreateItemms < ActiveRecord::Migration[5.2]
+  def change
+    create_table :itemms do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
